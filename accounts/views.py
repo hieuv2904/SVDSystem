11e3ec8b90cfc9453d23f79b1407e0c804ab9c3f
Hieu Vu <hieuv2904@gmail.com>
2024-05-07 16:32:12 +0700

    login

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,46 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+
+from .forms import CreateUserForm
 
 # Create your views here.
+
+def loginPage(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            redirect('home')
+        else:
+            messages.info(request, "Username or password is incorrect")
+
+    context = {}
+    return render(request, 'accounts/login.html', context)
+
+def registerPage(request):
+    form = CreateUserForm()
+
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.cleaned_data.get('username')
+            messages.success(request, "Account was created for " + user)
+
+            return redirect('login')
+
+    context = {"form": form}
+    return render(request, "accounts/register.html", context)
+
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
+
+def home(request):
+    return render(request, 'accounts/home.html')
